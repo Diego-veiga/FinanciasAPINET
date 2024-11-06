17004a9185c61750520902d2a38ca9187aed5f8c
Diego Veiga <diegoroberto27@hotmail.com>
2024-11-06 20:37:37 -0300

    test: create teste for CreateUserCommand

--- financiastests/validations/User/CreateUserCommandValidationTests.cs
+++ financiastests/validations/User/CreateUserCommandValidationTests.cs
@@ -1,5 +1,7 @@
 ﻿using AutoFixture;
+using financias.src.commands.BankAccount;
 using financiasapi.src.commands.user;
+using financiasapi.src.validations.BankAccount;
 using financiasapi.src.validations.User;
 using Xunit;
 
@@ -29,6 +31,118 @@ namespace financiastests.validations.User
 
             Assert.True(result.IsValid);
             Assert.Equal(result.Errors.Count, 0);
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithNameFieldGreaterThanThirty_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "Teste 123456789asdfghjklçpoiuytrewqazxcvbnmnqwertyuiopçlkj";
+            createUserCommand.Email = "teste123@hotmail.com";
+            createUserCommand.Password = "1234";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "The name of the bank must have a maximum of 30 characters");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithNameFieldNull_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = null;
+            createUserCommand.Email = "teste123@hotmail.com";
+            createUserCommand.Password = "1234";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "'Name' must not be empty.");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithNameFielEmpty_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "  ";
+            createUserCommand.Email = "teste123@hotmail.com";
+            createUserCommand.Password = "1234";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "'Name' must not be empty.");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithPassawordFieldLessThanFour_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "Teste";
+            createUserCommand.Email = "teste123@hotmail.com";
+            createUserCommand.Password = "12";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "The user's password must have a minimum of 4 characters and a maximum of 30 characters.");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithPassawordFieldGreaterThanThirty_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "Teste";
+            createUserCommand.Email = "teste123@hotmail.com";
+            createUserCommand.Password = "0123456789012345678901234567890123456789";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "The user's password must have a minimum of 4 characters and a maximum of 30 characters.");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithEmailFieldInvalid_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "Teste";
+            createUserCommand.Email = "teste";
+            createUserCommand.Password = "01234";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "Please enter a valid email address.");
+        }
+
+        [Fact]
+        public void CreateUserCommandValidation_ObjectWithEmailFieldNull_ReturnValidFalse()
+        {
+            var createUserCommand = _fixture.Create<CreateUserCommand>();
+            createUserCommand.Name = "Teste";
+            createUserCommand.Email = null;
+            createUserCommand.Password = "01234";
+            var createUserCommandValidation = new CreateUserCommandValidation();
+
+            var result = createUserCommandValidation.Validate(createUserCommand);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(result.Errors.Count, 1);
+            Assert.Equal(result.Errors[0].ErrorMessage, "'Email' must not be empty.");
         }
     }
 }
